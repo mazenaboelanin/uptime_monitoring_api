6a891f2f5343bea8b769bde2106a9443ee5081df
Mazen <mazenaboelanin@gmail.com>
2022-02-07 00:55:17 +0200

    database connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const dbConnection = require('./config/db');
 const dotenv = require('dotenv').config({path: './config/config.env'});
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Database Connection
+dbConnection();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Server is running on Port ${port}!`));
