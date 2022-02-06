b06b06d4522385179cb2c8d1e15cd37c00088253
Mazen <mazenaboelanin@gmail.com>
2022-02-07 00:58:44 +0200

    add public middlewares json and morgan

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const dbConnection = require('./config/db');
+ // Third party logger 
+ const morgan = require('morgan');
 const dotenv = require('dotenv').config({path: './config/config.env'});
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Public Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 
 // Database Connection
 dbConnection();
