bb26ed7bf0e93f15fc8dd0633442bbb5993ef614
Mazen <mazenaboelanin@gmail.com>
2022-02-12 14:49:36 +0200

    add - Get Successfull visits and Get Unsuccessul visits

--- src/controllers/visits.controller.js
+++ src/controllers/visits.controller.js
@@ -1,7 +1,7 @@
 const Visit = require("../models/Visit.model");
 
 // @ desc       Get All visits
-// @ route      GET api/v1/users
+// @ route      GET api/v1/visits
 // @ access     Private
 exports.getAllVisits = async(req, res, next)=>{
     try {
@@ -35,6 +35,46 @@ exports.getSingleVisit = async(req, res, next)=>{
             res.json({success: true, msg: "Get Single visit", visit});
         }else {
             res.json({success: false, msg: `No visit found with this ${id}`});
+        }
+    } catch (err) {
+        res.json({success: false, msg: "Error", err});
+    }
+}
+
+
+// @ desc       Get All Successfull visits
+// @ route      GET api/v1/visits/success
+// @ access     Private
+exports.getAllSuccessVisits = async(req, res, next)=>{
+    try {
+        const successvisits = await Visit.find({Successful: true});
+        if(successvisits.length > 0 ){
+            console.log('*** ', req.user._id.toString());
+            // Return All Visits Owned By logged in User
+            const visitsOwned = successvisits.filter(visit => visit.createdBy.toString() === req.user._id.toString());
+            res.json({success: true, msg: "Get All Successful visits", count: visitsOwned.length, visitsOwned});
+        }else {
+            res.json({success: false, msg: "No visits found"});
+        }
+    } catch (err) {
+        res.json({success: false, msg: "Error", err});
+    }
+}
+
+
+// @ desc       Get All UN Successfull visits
+// @ route      GET api/v1/visits/unsuccess
+// @ access     Private
+exports.getAllUnSuccessVisits = async(req, res, next)=>{
+    try {
+        const unSuccessvisits = await Visit.find({Successful: false});
+        if(unSuccessvisits.length > 0 ){
+            console.log('*** ', req.user._id.toString());
+            // Return All Visits Owned By logged in User
+            const visitsOwned = unSuccessvisits.filter(visit => visit.createdBy.toString() === req.user._id.toString());
+            res.json({success: true, msg: "Get All un Successful visits", count: visitsOwned.length, visitsOwned});
+        }else {
+            res.json({success: false, msg: "No visits found"});
         }
     } catch (err) {
         res.json({success: false, msg: "Error", err});
--- src/routes/visits.routes.js
+++ src/routes/visits.routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { getAllVisits, getSingleVisit} = require('../controllers/visits.controller');
+const { getAllVisits, getSingleVisit, getAllUnSuccessVisits, getAllSuccessVisits} = require('../controllers/visits.controller');
 const { protect } = require('../middleware/auth');
-
 
 router.route('/').get(protect, getAllVisits);
 router.route('/:id').get(protect, getSingleVisit);
+router.route('/info/success').get(protect, getAllSuccessVisits);
+router.route('/info/unsuccess').get(protect, getAllUnSuccessVisits);
 
 
 module.exports = router;
